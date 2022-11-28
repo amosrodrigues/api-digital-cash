d205a9740cf4b337f0ff36c57d3fc2188dda93ca
Amós Rodrigues <amos.adm.rh@gmail.com>
2022-11-28 13:52:50 -0300

    chore: Remove dist

--- dist/routes/authenticate.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.authenticateRoutes = void 0;
-const express_1 = require("express");
-const AuthenticateUserControler_1 = require("../modules/accounts/useCases/authenticateUser/AuthenticateUserControler");
-const authenticateRoutes = (0, express_1.Router)();
-exports.authenticateRoutes = authenticateRoutes;
-const authenticateUserController = new AuthenticateUserControler_1.AuthenticateUserController();
-authenticateRoutes.post('/sessions', authenticateUserController.handle);
