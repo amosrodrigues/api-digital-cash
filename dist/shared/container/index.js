85f0d7f114b6a07f8eae0160434c4ceecf46f12c
Amós Rodrigues <amos.adm.rh@gmail.com>
2022-12-17 11:00:01 -0300

    fix: remove directory dist.

--- dist/shared/container/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tsyringe_1 = require("tsyringe");
-const UsersRepository_1 = require("../../modules/accounts/repositories/implementations/UsersRepository");
-const TransactionsRepository_1 = require("../../modules/transactions/repositories/implementations/TransactionsRepository");
-const AccountsRepository_1 = require("../../modules/accounts/repositories/implementations/AccountsRepository");
-tsyringe_1.container.registerSingleton('transactionsRepository', TransactionsRepository_1.TransactionsRepository);
-tsyringe_1.container.registerSingleton('accountsRepository', AccountsRepository_1.AccountsRepository);
-tsyringe_1.container.registerSingleton('UsersRepository', UsersRepository_1.UsersRepository);
